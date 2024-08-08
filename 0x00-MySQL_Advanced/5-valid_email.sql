7d7c00a67c7dbad4fa9674f83ce68765fb0300b0
PaulaMaina <kabura.mainap@gmail.com>
2024-08-08 11:45:48 +0000

    Valid email trigger

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger that resets valid_email only when email is changed
+DELIMITER $$
+CREATE TRIGGER update_email
+BEFORE UPDATE ON users
+FROM EACH ROW
+BEGIN
+	IF OLD.email != NEW.email THEN
+	    SET NEW.valid_email = 0;
+	ELSE
+	    SET NEW.valid_email = NEW.valid_email;
+	END IF;
+END $$
+DELIMITER ;
