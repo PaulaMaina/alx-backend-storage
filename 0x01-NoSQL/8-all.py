6628a02cb0a5924446469a80ad085e19e6e980bb
PaulaMaina <kabura.mainap@gmail.com>
2024-08-13 13:06:58 +0000

    All

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -9,10 +9,7 @@ def list_all(mongo_collection):
     """Returns a list of documents in a collection"""
     doc_list = []
 
-    client = MongoClient()
-    col = db["mongo_collection"]
-
-    for doc in col.find():
+    for doc in mongo_collection.find():
         doc_list.append(doc)
 
     return doc_list
