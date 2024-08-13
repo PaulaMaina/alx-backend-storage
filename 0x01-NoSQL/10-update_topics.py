5358f36f055c2cfcdf3a92079e404bdaf875adb3
PaulaMaina <kabura.mainap@gmail.com>
2024-08-13 13:19:53 +0000

    Update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Changes all topics of a school document"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Updates topics that match name"""
+    query = {name: name}
+    new_topics = {"$set": {topics: topics}}
+
+    monog_collection.update_many(query, new_topics)
