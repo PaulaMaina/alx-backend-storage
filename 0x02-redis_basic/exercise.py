75c802a7ca0ce52e204bef390585a8d01d3fdd27
PaulaMaina <kabura.mainap@gmail.com>
2024-08-14 10:23:01 +0000

    Count calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 """Redis Basics"""
-from typing import Callable, Union
+from functools import wraps
+from typing import Any, Callable, Union
 import redis
 import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    """Keeps track of how many times Cache methods are called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        """Returns the methos after incrementing the counter"""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -11,7 +23,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores a value in a Redis database and returns the key"""
         key_data = str(uuid.uuid4())
@@ -28,11 +40,9 @@ class Cache:
         val = self._redis.get(key)
         return fn(val) if fn is not None else val
 
-
     def get_str(self, key: str) -> str:
         """Retrieves a string value related to the key"""
         return self.get(key, lambda x: x.decode("utf-8"))
-
 
     def get_int(self, key: str) -> int:
         """Retrieves an integer related to the key"""
